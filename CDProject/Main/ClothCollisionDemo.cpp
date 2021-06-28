b7ba757b17bb6f7cd5824c7141e905d3623752c9
hodolyi57 <speeedm1@naver.com>
2021-06-28 11:18:48 +0900

    fixed the problem when the mouse is out of the area

--- CDProject/Main/ClothCollisionDemo.cpp
+++ CDProject/Main/ClothCollisionDemo.cpp
@@ -193,35 +193,53 @@ int main(int argc, char** argv)
 		ImGui::SliderFloat("trans_z", &trans_z, -300.0f, 300.0f);
 
 		// move scene using mouse drag with left button
-		ImVec2 diff_left = ImGui::GetMouseDragDelta(0);
+		if (ImGui::GetIO().MouseClickedPos[0].x > 180.0f && ImGui::GetIO().MouseClickedPos[0].x < 1536.0f)
+		{
+			if (ImGui::GetIO().MouseClickedPos[0].y > 20 && ImGui::GetIO().MouseClickedPos[0].y < 452)
+			{
+				ImVec2 diff_left = ImGui::GetMouseDragDelta(0, 0.0f);
+				trans_x += -diff_left[0] / static_cast<Real>(20.0);
+				trans_y += diff_left[1] / static_cast<Real>(20.0);
+			}
+		}
 		ImGui::ResetMouseDragDelta(0);
-		trans_x += -diff_left[0] / static_cast<Real>(20.0);
-		trans_y += diff_left[1] / static_cast<Real>(20.0);
 
 		// zoom in or out using mouse wheel
-		float wheelcounter = ImGui::GetIO().MouseWheel;
-		trans_z += -(wheelcounter) / static_cast<Real>(10.0);
+		if (ImGui::GetIO().MousePos.x > 180.0f && ImGui::GetIO().MousePos.x < 1536.0f)
+		{
+			if (ImGui::GetIO().MousePos.y > 20 && ImGui::GetIO().MousePos.y < 452)
+			{
+				float wheelcounter = ImGui::GetIO().MouseWheel;
+				trans_z += -(wheelcounter) / static_cast<Real>(1.0);
+			}
+		}
 
 		MiniGL::m_translation.x() = -(Real)trans_x;
 		MiniGL::m_translation.y() = -(Real)trans_y;
 		MiniGL::m_translation.z() = -(Real)trans_z;
 
 		// rotate scene using mouse drag with right button
-		ImVec2 diff_right = ImGui::GetMouseDragDelta(1);
-		ImGui::ResetMouseDragDelta(1);
-		if (rot_x + diff_right[1] > 360.0f)
-			rot_x = 360.0f;
-		else if (rot_x + diff_right[1] < -360.0f)
-			rot_x = -360.0f;
-		else
-			rot_x += diff_right[1];
+		if (ImGui::GetIO().MouseClickedPos[1].x > 180.0f && ImGui::GetIO().MouseClickedPos[1].x < 1536.0f)
+		{
+			if (ImGui::GetIO().MouseClickedPos[1].y > 20 && ImGui::GetIO().MouseClickedPos[1].y < 452)
+			{
+				ImVec2 diff_right = ImGui::GetMouseDragDelta(1, 0.0f);
+				if (rot_x + diff_right[1] > 360.0f)
+					rot_x = 360.0f;
+				else if (rot_x + diff_right[1] < -360.0f)
+					rot_x = -360.0f;
+				else
+					rot_x += diff_right[1];
 
-		if (rot_y - diff_right[0] > 360.0f)
-			rot_y = 360.0f;
-		else if (rot_y - diff_right[0] < -360.0f)
-			rot_y = -360.0f;
-		else
-			rot_y -= diff_right[0];
+				if (rot_y - diff_right[0] > 360.0f)
+					rot_y = 360.0f;
+				else if (rot_y - diff_right[0] < -360.0f)
+					rot_y = -360.0f;
+				else
+					rot_y -= diff_right[0];
+			}
+		}
+		ImGui::ResetMouseDragDelta(1);
 
 		//ImGui::SliderFloat("rot_w", &rot_w, 0.0f, 1.0f);
 		ImGui::SliderFloat("rot_x", &rot_x, -180.0f, 180.0f);
